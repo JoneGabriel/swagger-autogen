20917ec43f07024149f9199ea78a668b8598a211
JoaoGabriel_21 <61243774+JoneGabriel@users.noreply.github.com>
2022-07-04 22:11:09 -0300

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -1,7 +1,7 @@
 const swaggerAutogen = require('swagger-autogen')();
 
-const outputFile = './swagger_output.json';
-const endpointsFiles = ['./route.js'];
+const outputFile = './swagger_output.json'; //json vazio 
+const endpointsFiles = ['./route.js']; //arquivo onde estão as rotas
 
 swaggerAutogen(outputFile, endpointsFiles).then(()=>{
     require('./index.js');
